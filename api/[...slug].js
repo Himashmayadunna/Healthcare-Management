26ed445ece91af9dc4a09210dc5975d2646894d8
Himashmayadunna <154499295+Himashmayadunna@users.noreply.github.com>
2026-05-03 07:20:58 +0530

    adding deployments

--- api/[...slug].js
+++
@@ -1,9 +0,0 @@
-/**
- * Vercel Serverless Function Handler for Backend
- * Route: /api/[...slug].js
- * This proxies all /api/* requests to the Express backend
- */
-
-const app = require("../../backend/server");
-
-module.exports = app;
